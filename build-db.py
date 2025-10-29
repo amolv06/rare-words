0ae6647b5605f6c2cdad294808bb56a54c9b6d27
Amol Vaidya <amolvaidya06@gmail.com>
2025-10-29 09:26:40 -0500

    Lowercased everything in building the sqlite database

--- build-db.py
+++ build-db.py
@@ -13,8 +13,11 @@ lemma_freq_df = df.groupby('LEMMA', as_index=False)['FREQUENCY'] \
                   .sort_values('sum_freq', ascending=False)
 lemma_freq_df['rank'] = lemma_freq_df['sum_freq'].rank(method='min',
                                                   ascending=False).astype(int)
+lemma_freq_df["LEMMA"] = lemma_freq_df["LEMMA"].str.lower()
 lemma_forms_df = df.assign(INFLECTIONS=df['INFLECTIONS'].fillna('')  
                  .astype(str).str.split(r'\s*,\s*')).explode('INFLECTIONS')
+lemma_forms_df["LEMMA"] = lemma_forms_df["LEMMA"].str.lower()
+lemma_forms_df["INFLECTIONS"] = lemma_forms_df["INFLECTIONS"].str.lower()
 lemma_forms_df = lemma_forms_df.loc[lemma_forms_df['INFLECTIONS'] != ''] \
                                .drop_duplicates(['LEMMA', 'INFLECTIONS'])
 forms_rank_df = lemma_forms_df.merge(lemma_freq_df, on='LEMMA', how='left') \
